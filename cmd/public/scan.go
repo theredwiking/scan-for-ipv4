6984ea5aac4881e54bb27457826eb6d36dae3892
theredwiking <hjulmandn@gmail.com>
2022-10-22 22:00:47 +0200

    Added load bar

--- cmd/public/scan.go
+++ cmd/public/scan.go
@@ -4,23 +4,30 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/gosuri/uiprogress"
 	"github.com/theredwiking/scan-for-ipv4/cmd/utils"
 )
 
 func Scanner() {
 	utils.CheckFile()
 
+	uiprogress.Start()            // start rendering
+	bar := uiprogress.AddBar(100) // Add a new bar
+
 	first, second, third, fourth := 1, 1, 1, 0
 
+	bar.AppendCompleted()
+	bar.PrependElapsed()
+	
 	for true {
-
+		
 		for fourth < 255 {
 			fourth++
 			ipAddr := fmt.Sprintf("%d.%d.%d.%d", first, second, third, fourth)
-
+			
 			utils.PingFunc(ipAddr)
 		}
-
+		bar.Incr()
 		fourth = 0
 
 		if third != 255 {
--- go.mod
+++ go.mod
@@ -6,7 +6,10 @@ require github.com/go-ping/ping v1.1.0
 
 require (
 	github.com/google/uuid v1.3.0 // indirect
+	github.com/gosuri/uilive v0.0.4 // indirect
+	github.com/gosuri/uiprogress v0.0.1 // indirect
 	github.com/inconshreveable/mousetrap v1.0.1 // indirect
+	github.com/mattn/go-isatty v0.0.16 // indirect
 	github.com/spf13/cobra v1.6.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	golang.org/x/net v0.1.0 // indirect
