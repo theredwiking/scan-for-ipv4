c4b613591fd6c77fab7bb772b3e02b3b1193c089
theredwiking <hjulmandn@gmail.com>
2022-11-06 22:11:47 +0100

    adding get request

--- cmd/calculator/calculate.go
+++ cmd/calculator/calculate.go
@@ -3,7 +3,10 @@ package calculator
 import (
 	"errors"
 	"fmt"
+	"io/ioutil"
 	"net"
+	"net/http"
+	"net/url"
 	"strings"
 
 	"github.com/theredwiking/scan-for-ipv4/cmd/utils"
@@ -32,13 +35,38 @@ func Calc() {
 
 	if err != nil {
 		fmt.Println(err)
+		return
 	}
 
 	calculate(ip, subnet)
 }
 
 func calculate(baseip string, subnet string) {
-	
+	params := url.Values{}
+
+	params.Add("cclass", "any")
+	params.Add("csubnet", subnet)
+	params.Add("cip", baseip)
+	params.Add("ctype", "ipv4")
+	params.Add("x", "0")
+	params.Add("y", "0")
+
+	resp, err := http.Get("https://www.calculator.net/ip-subnet-calculator.html?" + params.Encode())
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	body, err := ioutil.ReadAll(resp.Body) // Log the request body
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	bodyString := string(body)
+	fmt.Println(bodyString)
 }
 
 func localIp() (string, string, error) {
@@ -57,11 +85,12 @@ func localIp() (string, string, error) {
 
 		if err != nil {
 			fmt.Println(err)
+			return "", "", err
 		}
 
-		info := strings.Split(fmt.Sprintf("%s", addr[0]), "/")
+		info := strings.Split(addr[0].String(), "/")
 
 		return info[0], info[1], nil
 	}
-	return "", "", errors.New("Are you even connected to a network?")
+	return "", "", errors.New("are you even connected to a network?")
 }
